814aa427b330c6163997aa51eb27e1263b2f2b35
Khanh Hua <khanh.hua@doctari.de>
2022-06-13 23:45:48 +0200

    Map to column records

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,16 @@ module Main where
 import Data.List (intersperse)
 
 type Header = String
+type Formatted = String
 
 data Record = Record
   { no :: String
   , name :: String
+  }
+
+data Column = Column
+  { header :: Header
+  , cells :: [Formatted]
   }
 
 records :: [Record]
@@ -15,10 +21,25 @@ records =
 
 main :: IO ()
 main = do
-  putStrLn $ tableAscii ["no", "name"] records
+  let
+    columns =
+      [ Column "no" $ map no records
+      , Column "name" $ map name records
+      ]
+  putStrLn $ columnsAscii columns
+
+
+columnsAscii :: [Column] -> String
+columnsAscii columns = headerRow <> "\n" <> body
+  where
+    headerRow = unwords $ intersperse " | " $ map header columns
+    rows = map cells columns
+    body = unwords $ zipWith (curry f) (head rows) (rows!!1)
+    f (a, b) = a <> " | " <> b
+
 
 tableAscii :: [Header] -> [Record] -> String
 tableAscii headers items = headerRow <> "\n" <> concatMap f items
   where
-    headerRow = unwords $ intersperse " | "  headers 
+    headerRow = unwords $ intersperse " | "  headers
     f (Record no name) = no <> " | " <> name <> "\n"
